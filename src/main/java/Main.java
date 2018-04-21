631826f2806c8190f191b6a0550e1da974e258b6
yixiang <yixiangd1118@gmail.com>
2018-04-20 17:25:39 -0700

    parseAll() done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,11 @@
+import org.apache.tika.exception.TikaException;
+import org.xml.sax.SAXException;
+
+import java.io.IOException;
+
 public class Main {
+    public static void main(String args[]) throws TikaException, IOException, SAXException {
+        Parser parser = new Parser("/Users/yixiangding/Sites/csci572_solr/FOX_News/HTML_files");
+        parser.parseAll();
+    }
 }
--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,2 +1,40 @@
+import java.io.*;
+
+import org.apache.tika.exception.TikaException;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.parser.html.HtmlParser;
+import org.apache.tika.sax.BodyContentHandler;
+import org.xml.sax.SAXException;
+
 public class Parser {
+    private String htmlFolderPath = "/";
+
+    public Parser(String path) {
+        htmlFolderPath = path;
+    }
+
+    public void parseAll() throws IOException, TikaException, SAXException {
+        File folder = new File(htmlFolderPath);
+        for (File file : folder.listFiles()) {
+            parseContent(file);
+        }
+    }
+
+    private void parseContent(File htmlFile) throws IOException, TikaException, SAXException {
+        //detecting the file type
+        BodyContentHandler handler = new BodyContentHandler(-1);
+        Metadata metadata = new Metadata();
+        FileInputStream inputstream = new FileInputStream(htmlFile);
+        ParseContext pcontext = new ParseContext();
+
+        //Html parser
+        HtmlParser htmlparser = new HtmlParser();
+        htmlparser.parse(inputstream, handler, metadata, pcontext);
+        System.out.println("Contents of the document:" + handler.toString());
+        BufferedWriter writer = new BufferedWriter(new FileWriter("big.txt"));
+        writer.write(handler.toString().trim());
+        writer.flush();
+        writer.close();
+    }
 }
